d1182c897388bf2cb3aeb2cd6715fa59f159ad85
Navin Sadanandan <navinds25@gmail.com>
2021-07-17 09:04:12 +0530

    added http srv

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const puppeteer = require('puppeteer');
+const fastify = require('fastify');
+const app = fastify({
+    logger: true
+});
+
 /* 
 The function below scrapes hotstar for the f1 video urls, so we can avoid spoilers in the images and hotstar home page.
 */
-(async () => {
+async function fetchURLs() {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto('https://www.hotstar.com/in/sports/formula-1', { 'timeout': 10000, 'waitUntil': 'domcontentloaded' });
@@ -17,4 +22,29 @@ The function below scrapes hotstar for the f1 video urls, so we can avoid spoile
     await page.close();
     await browser.close();
     console.log(urls);
-})();
+    return urls;
+};
+
+app.get('/', async (req, res) => {
+    const baseURL = 'https://www.hotstar.com'
+    const rawUrls = await fetchURLs();
+    let content = '<html><body>';
+    rawUrls.forEach(element => {
+        const url = baseURL + element;
+        console.log(url);
+        content = content + '<a href="' + url + '">' + url + '</a><br/>';
+    });
+    const closeContent = '</body></html>';
+    content = content + closeContent;
+    console.log(content);
+    res.type('text/html');
+    res.send(content);
+});
+
+app.listen(3000, function (err, address) {
+    if (err) {
+        console.error(err)
+        process.exit(1)
+    }
+    console.info(`server listening on ${address}`)
+});
